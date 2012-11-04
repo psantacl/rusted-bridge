c3e4b683f4e5b77067baadaff5d9403c64508a30
Paul Santa Clara <psantaclara@relaynetwork.com>
2012-11-04 15:57:45 -0500

    basic server socket functionality

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject rusted-bridge "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"] ]
-  :plugins [[lein-swank "1.4.4"]]
-)
+  :dependencies [[org.clojure/clojure "1.4.0" ] 
+                 [io.netty/netty "4.0.0.Alpha1-SNAPSHOT"]
+                 [org.clojars.kyleburton/clj-etl-utils "1.0.40" ] ]
+  :plugins [[lein-swank "1.4.4"]])
--- src/rusted_bridge/core.clj
+++ src/rusted_bridge/core.clj
@@ -1,1 +1,45 @@
-(ns rusted-bridge.core)
+(ns rusted-bridge.core
+  (:use
+   [clj-etl-utils.lang-utils :only [raise]])  
+  (:import
+   [io.netty.bootstrap ServerBootstrap]
+   [io.netty.channel.socket.nio NioServerSocketChannelFactory]  
+   [io.netty.channel ChannelPipelineFactory Channels SimpleChannelUpstreamHandler ChannelFutureListener]
+   [io.netty.buffer ChannelBuffers]
+   [java.net InetSocketAddress]
+   [java.util.concurrent Executors]))
+
+
+(def config {:port 9000})
+
+(defn make-handler []
+  (proxy [ SimpleChannelUpstreamHandler] []
+    (channelConnected [ctx e]
+      (println "channel connected event"))
+    (messageReceived [ctx e]
+      (let [msg (.getMessage e)]
+        (-> (.getChannel ctx) (.write msg))))
+    (exceptionCaught [ctx e]
+      (println "exception was encountered :("))))
+
+(defn start-netty-server []
+  (let [ch-factory (NioServerSocketChannelFactory. (Executors/newCachedThreadPool)
+                                                   (Executors/newCachedThreadPool))
+        bootstrap       (ServerBootstrap. ch-factory)
+        pl-factory      (reify ChannelPipelineFactory
+                          (getPipeline [this]
+                            (doto (Channels/pipeline)
+                              (.addLast "handler"  (make-handler )))))]
+    (.setPipelineFactory bootstrap pl-factory)
+    (.setOption bootstrap "child.tcpNoDelay" true)
+    (.setOption bootstrap "child.keepAlive" true)
+    (.bind bootstrap (InetSocketAddress. (:port config)) )))
+
+
+(comment
+
+  (def server (start-netty-server))
+  
+
+  (.close server)
+  )
