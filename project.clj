9e86ce774704742588c3eb91c092eaf8bb806d42
Paul Santa Clara <psantaclara@relaynetwork.com>
2012-11-04 15:34:12 -0500

    setting up project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject rusted-bridge "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"] ]
+  :plugins [[lein-swank "1.4.4"]]
+)
