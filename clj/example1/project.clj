68e8999f72b43eb1868c377a88ba121c3d84b917
Paul Santa Clara <psantaclara@relaynetwork.com>
2013-04-06 21:56:36 -0400

    reorganizing things

---
+++ clj/example1/project.clj
@@ -0,0 +1,5 @@
+(defproject example1 "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                [org.wol/rusted-bridge "1.0.0"] ]
+  :plugins [[lein-swank "1.4.4"]])
---
+++ clj/example1/test/example1/test/core.clj
@@ -0,0 +1,6 @@
+(ns example1.test.core
+  (:use [example1.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
